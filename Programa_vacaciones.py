435fcda112ef64afb721c1d414472c45a6d2a85a
Emmanuel <emmanueldc95@gmail.com>
2020-07-19 16:35:17 -0500

    Update Programa_vacaciones.py

--- Programa_vacaciones.py
+++ Programa_vacaciones.py
@@ -6,48 +6,50 @@ nombre = input('\n¿Cual es tu nombre?: ')
 clave = int(input('\n¿Cual es la clave de tu departamento?: '))
 antiguedad = int(input('\n¿Cuantos años tienes trabajando en la empresa?: '))
 
-if clave == 1 and antiguedad == 1:
-    print('\nFelicidades', nombre, 'tienes 6 dias de vacaciones')
-    print('\nFin.')
+if clave == 1:
 
-elif clave == 1 and antiguedad <= 6:
-    print('\nFeliciades', nombre, 'tienes 14 dias de vacaciones')
-    print('\nFin.')
+    if antiguedad == 1:
+        print('\nFelicidades', nombre, 'tienes 6 dias de vacaciones')
+        print('\nFin.')
 
-elif clave == 1 and antiguedad >= 7:
-    print('\nFelicidades', nombre, 'tienes 20 dias de vacaciones\n')
-    print('Fin')
+    elif antiguedad <= 6:
+        print('\nFeliciades', nombre, 'tienes 14 dias de vacaciones')
+        print('\nFin.')
 
-else:
-    
-    if clave == 2 and antiguedad == 1:
+    elif antiguedad >= 7:
+        print('\nFelicidades', nombre, 'tienes 20 dias de vacaciones\n')
+        print('Fin')
+
+elif clave == 2:
+
+    if antiguedad == 1:
         print('\nFelicidades', nombre, 'tienes 7 dias de vacaciones')
         print('\nFin.')
 
-    elif clave == 2 and antiguedad <= 6:
+    elif antiguedad <= 6:
         print('\nFeliciades', nombre, 'tienes 15 dias de vacaciones')
         print('\nFin.')
     
-    elif clave == 2 and antiguedad >= 7:
+    elif antiguedad >= 7:
         print('\nFelicidades', nombre, 'tienes 22 dias de vacaciones')
         print('\nFin.')
 
-    else:
+elif clave == 3:
 
-        if clave == 3 and antiguedad == 1:
-            print('\nFelicidades', nombre, 'tienes 10 dias de vacaciones')
-            print('\nFin.')
+    if antiguedad == 1:
+        print('\nFelicidades', nombre, 'tienes 10 dias de vacaciones')
+        print('\nFin.')
 
-        elif clave == 3 and antiguedad <= 6:
-            print('\nFeliciades', nombre, 'tienes 20 dias de vacaciones')
-            print('\nFin.')
+    elif antiguedad <= 6:
+        print('\nFeliciades', nombre, 'tienes 20 dias de vacaciones')
+        print('\nFin.')
 
-        elif clave == 3 and antiguedad >= 7:
-            print('\nFelicidades', nombre, 'tienes 30 dias de vacaciones')
-            print('\nFin.')
-        else:
-            print('Sin derecho a vacaciones')
-            print('Fin.')
+    elif antiguedad >= 7:
+        print('\nFelicidades', nombre, 'tienes 30 dias de vacaciones')
+        print('\nFin.')
+
+else:
+    print('La clave de departamento no existe, vuelve a intentarlo')
 
             
             
